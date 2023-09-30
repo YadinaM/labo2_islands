b28ba8f63e7039a5922fdb102b2ba7237d3c0283
YadinaM <yadina.moreira@gmail.com>
2023-09-30 12:52:23 +0200

    feat: save color of islands

--- islands/src/Save.js
+++ islands/src/Save.js
@@ -1,3 +1,20 @@
+import Island from "./Island.js"
+
 export default class Save{
-    
+    constructor(){
+
+        document.querySelector('.save').addEventListener('click', (e) => {
+            const islandElements = document.querySelectorAll(".island");
+            const islandsData = [];
+
+            islandElements.forEach((islandElement) => {
+                const islandData = {
+                  color: islandElement.style.backgroundColor,
+                };
+                islandsData.push(islandData);
+              });
+              localStorage.setItem("islands", JSON.stringify(islandsData));
+              console.log("Islands saved to localStorage:", islandsData);
+        })
+    }
 }

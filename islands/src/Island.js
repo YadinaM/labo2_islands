e4a4419b0630ff03bdf1b48e09f60f10576fbbaa
YadinaM <yadina.moreira@gmail.com>
2023-09-30 12:10:04 +0200

    feat: add island in random spot on screen

--- islands/src/Island.js
+++ islands/src/Island.js
@@ -1,20 +1,30 @@
 export default class Island{
+
+    getRandomCoords(){
+        let x = Math.floor(Math.random()*window.innerWidth);
+        let y = Math.floor(Math.random()*window.innerHeight);
+        return{x,y};
+    }
+
     constructor(){
-        console.log("hey");
-        this.hookEvents();
-    }
 
-    hookEvents(){
-        document.querySelector("#add-item-text").addEventListener("keyup", (e) => {
-            if (e.key === "Enter") {
-                this.addItem(e.target.value);
-            }
+        document.querySelector(".add").addEventListener('click', (e) => {
+            let div = document.createElement("div");
+            div.classList.add("island");
+            document.body.appendChild(div);
+            console.log(div);
+
+
+            let coords = this.getRandomCoords();
+            console.log(coords.x);
+            div.animate([
+                {transform: `translate(0px, 0px)`},
+                {transform: `translate(${coords.x}px, ${coords.y}px)`}
+            ], {
+                duration: 1000,
+                iterations: 1,
+                fill: "forwards"
+            });
         })
-    }
-
-    addItem(text){
-        //console.log(`Add item: ${text}`);
-        let todo = new Todo(text);
-        todo.render();
     }
 }

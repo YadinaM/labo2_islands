f641248d7df2da357e03f711323d89204615137f
YadinaM <yadina.moreira@gmail.com>
2023-09-30 12:56:50 +0200

    feat: add load class

--- islands/main.js
+++ islands/main.js
@@ -1,5 +1,8 @@
 import "./style.css";
 import Island from "/src/Island.js";
 import Save from "/src/Save.js";
+import Load from "/src/Load.js";
+
 const island = new Island();
 const save = new Save();
+const load = new Load();
